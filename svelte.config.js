6a354d87cbd5817c1eae9d6c991e4808f7caf302
Kolja <lasar@rasal.de>
2025-01-07 11:44:20 +0100

    chore: update base path for production and upgrade dependencies in package.json

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		adapter: adapter()
 	},
 	paths: {
-		base: process.env.NODE_ENV === 'production' ? '/Svelte-ReaPer-Store' : ''
+		base: process.env.NODE_ENV === 'production' ? '/Svelte-5-ReaPer-Store' : ''
 	}
 };
 
